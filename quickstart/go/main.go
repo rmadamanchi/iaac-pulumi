53ebd89f0f8723a2816a20adf46c59837b3bfcad
Rajesh Madamanchi <rmadamanchi@gmail.com>
2023-03-20 08:32:34 -0500

    first

---
+++ quickstart/go/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/acm"
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/s3"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Create an AWS resource (S3 Bucket)
+		bucket, err := s3.NewBucket(ctx, "my-bucket", nil)
+		if err != nil {
+			return err
+		}
+
+		cert, err := acm.NewCertificate(ctx, "cert", &acm.CertificateArgs{
+			DomainName:       pulumi.String("example"),
+			ValidationMethod: pulumi.String("DNS"),
+		})
+
+		// Export the name of the bucket
+		ctx.Export("bucketName", bucket.ID())
+		return nil
+	})
+}
