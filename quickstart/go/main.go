8387ca9f2790c502d2714cd824ccaefe3e250152
Rajesh Madamanchi <rmadamanchi@gmail.com>
2023-03-22 11:14:54 -0400

    trying for loops; more notes

--- quickstart/go/main.go
+++ quickstart/go/main.go
@@ -1,26 +1,31 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/acm"
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"strconv"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create an AWS resource (S3 Bucket)
-		bucket, err := s3.NewBucket(ctx, "my-bucket", nil)
-		if err != nil {
-			return err
+
+		buckets := make([]*s3.Bucket, 0)
+		for i := 0; i < 20; i++ {
+			bucket, err := s3.NewBucket(ctx, "my-bucket-"+strconv.Itoa(i), nil)
+			if err != nil {
+				return err
+			}
+			buckets = append(buckets, bucket)
 		}
 
-		cert, err := acm.NewCertificate(ctx, "cert", &acm.CertificateArgs{
-			DomainName:       pulumi.String("example"),
-			ValidationMethod: pulumi.String("DNS"),
-		})
-
 		// Export the name of the bucket
-		ctx.Export("bucketName", bucket.ID())
+		bucketIds := make([]pulumi.StringOutput, 0)
+		for _, bucket := range buckets {
+			bucketIds = append(bucketIds, bucket.Arn)
+		}
+
+		ctx.Export("Buckets", pulumi.ToStringArrayOutput(bucketIds))
 		return nil
 	})
 }
